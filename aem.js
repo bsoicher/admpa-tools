b0d7a7b7b2bb8c7354f32a17dcdd431058c2f654
Strix\Ben <ben.soicher@gmail.com>
2019-05-21 17:17:33 -0400

    trying ajax queue

--- aem.js
+++ aem.js
@@ -5,30 +5,34 @@
  * URL of the root node
  * @var {String} ROOT URL
  */
-var ROOT = 'https://www.canada.ca/en/department-national-defence/test/maple-leaf/'
+var root = 'https://www.canada.ca/en/department-national-defence/test/maple-leaf/'
 
 /**
  * URL structure of an aticle node (YYYY/MM/article.html)
  * @var {String}
  */
-var MATCH = /\/\d{4}\/\d{2}\/[^.]*\.html$/i
+var match = /\/\d{4}\/\d{2}\/[^.]*\.html$/i
 
 /**
- * Global object to hold downloaded metadata
+ * Global object to hold downloaded data
  * @var {Object} DATA
  */
-var DATA = {}
+var data = []
+
+function updatePreview() {
+  $('#preview').text(JSON.stringify(data, null, 2))
+}
 
 /**
- * Get node structure
+ * Load article list
  * @param {Function} callback
+ * @returns {jqXHR}
  */
-function get_list(callback) {
-
-  var a = $.ajax({
+function loadList() {
+  return $.get({
 
     // Add sitemap extension to root URL
-    url: ROOT + '.sitemap.xml',
+    url: root + '.sitemap.xml',
 
     // Response will be json once converted
     dataType: 'json',
@@ -38,32 +42,39 @@ function get_list(callback) {
       // Parse XML
       var xml = $.parseXML(data)
 
-      // Create array of paths, ignores nodes without YYYY/MM structure
+      // List URLs, ignores nodes that dont match structure
       var list = $(xml).find('loc').map(function () {
-        return MATCH.test(this.innerHTML) ? this.innerHTML : null
+        return match.test(this.innerHTML) ? this.innerHTML : null
       }).get()
 
       // Convert back to string
       return JSON.stringify(list)
     },
 
+    success: function (list) {
+      data = list
+      $('#preview').text(JSON.stringify(data, null, 2))
+    },
+
     // Log if an error occured
-    error: function () {
-      console.log('Failed to load sitemap')
+    error: function (e) {
+      data = []
+      updatePreview()
     }
   })
-
-  if ($.isFunction(callback)) {
-    a.then(callback)
-  }
 }
 
-function get_node(url) {
+/**
+ * Load node metadata
+ * @param {String} url Node URL
+ * @return {Object}
+ */
+function loadNode(url) {
 
   // Extract language from URL
   var lang = /canada.ca\/fr\//i.test(url) ? 'fr' : 'en'
 
-  $.ajax({
+  return $.ajax({
 
     // URL of JSON node data
     url: url.replace('.html', '/_jcr_content.json'),
@@ -71,7 +82,11 @@ function get_node(url) {
     // Process json response
     success: function (json) {
       // Basic node information
-      DATA[''].title = json['jcr:title'] || null
+var i ={}
+      //DATA[url] = {)
+        
+        
+        //title = json['jcr:title'] || null
       i.desc = json['gcDescription'] || null
       i.keywords = json['gcKeywords'] || null
       i.thumb = json['gcOGImage'] || null
@@ -82,10 +97,8 @@ function get_node(url) {
       i.published = new Date(json['gcLastPublished'])
       i.expires = new Date(json['gcDateExpired'])
 
-      // Create and link alternate node if it has not been created
-      if (!i.alt && json['gcAltLanguagePeer']) {
-        i.alt = new AEM_Node(json['gcAltLanguagePeer'].replace('/content/canadasite/', 'https://www.canada.ca/'), i)
-      }
+      i.alt = json['gcAltLanguagePeer']
+      
     },
 
     // Log if AJAX error occured
@@ -97,11 +110,3 @@ function get_node(url) {
 
 
 
-
-
-$(function () {
-  get_list(function (list) {
-    DATA = list
-    console.log(list)
-  })
-})
