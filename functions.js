219ccfe6dcbfa72df0166c72a61af8e186623bbb
Ben Soicher <ben.soicher@gmail.com>
2019-05-30 17:18:48 -0400

    testing

---
+++ functions.js
@@ -0,0 +1,26 @@
+
+function sitemap_url() {
+
+}
+
+
+
+var articleRegEx = /\/\d{4}\/\d{2}\/[^.]*\.html$/i
+
+function is_article(url) {
+    /\/\d{4}\/\d{2}\/[^.]*\.html$/i.test(url)
+}
+
+
+function format_date(date) {
+
+}
+
+
+
+function get_date(jcr) {
+
+    
+
+
+}
