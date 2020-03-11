c2f061d1f5f2727599553fa30debdc4e72ab1b2a
Strix\Ben <ben.soicher@gmail.com>
2020-03-11 15:01:59 -0400

    better toolbar

--- tools.user.js
+++ tools.user.js
@@ -1,86 +1,90 @@
 // ==UserScript==
 // @name         AEM Tools
-// @namespace    http://www.bensoicher.ca
-// @version      0.1
+// @namespace    https://caf-fac.ca/
+// @version      0.1.0
 // @updateURL    https://caf-fac.ca/tools/tools.user.js
-// @description  Toolbar of AEM shortcuts
+// @description  Tools to help with AEM publishing to canada.ca
 // @author       Ben Soicher
+// @include      http://52.204.44.53/*
 // @include      https://www.canada.ca/*
 // @include      https://author-canada-prod.adobecqms.net/*
-// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @run-at       document-end
 // ==/UserScript==
 
 (function ($) {
-  'use strict'
+
+  var server = 'http://52.204.44.53'
+  var url = window.location
+  var alt = GM_getValue('alt', false)
+  var autoPublish = '&autopublish'
+
+  // Check for auto close flag
+  if (document.referrer.includes('&autoclose')) { return window.close() }
+
+  // Check for auto publish flag
+  if (url.pathname.endsWith('/publishpagewizard.html') && url.href.includes('&autopublish')) {
+    $('form.cq-siteadmin-admin-publishpage-form').submit()
+  }
 
   if (window.location.href.includes('canada.ca')) {
-
     var node = window.location.href.replace('https://www.canada.ca/', '/content/canadasite/').replace('.html', '')
     var node_alt = '/content/canadasite' + $('#wb-lng a').attr('href').replace('.html', '')
 
-    var author = 'https://author-canada-prod.adobecqms.net'
-
-    var menu = '<ul id="aem_tools" class="btn-toolbar list-inline pull-right mrgn-tp-sm" role="toolbar">'
-    menu += '<li class="btn btn-link">View</li>'
+    var menu = '<ul id="aem_tools" class=" pull-right mrgn-tp-md" role="toolbar">'
+    menu += '<li class="btn-group mrgn-rght-sm"><button class="btn ' + (alt ? 'btn-danger' : 'btn-default') + ' brdr-rds-0" data-action="both" id="both">' + (alt ? 'Both' : 'Single') + '</button></li>'
     menu += '<li class="btn-group">'
-    menu += '<a class="btn btn-default" data-type="meta" target="_blank">Metadata</a>'
-    menu += '<a class="btn btn-default" data-type="folder" target="_blank">Folder</a>'
-    menu += '<a class="btn btn-default" data-type="qa" target="_blank">QA</a>'
-    menu += '</li>'
-    menu += '<li class="btn btn-link">Edit</li>'
-    menu += '<li class="btn-group">'
-    menu += '<a class="btn btn-default" data-type="edit" target="_blank">Content</a>'
-    menu += '<a class="btn btn-default" data-type="prop" target="_blank">Properties</a>'
-    menu += '</li>'
-    menu += '<li class="btn btn-link">Publish</li>'
-    menu += '<li class="btn-group">'
-    menu += '<a class="btn btn-default" data-type="publish" target="_blank">Production</a>'
+    menu += '<button class="btn btn-default brdr-rds-0" data-action="metadata" title="View metadata JSON">Metadata</button>'
+    menu += '<button class="btn btn-default brdr-rds-0" data-action="qa" title="Run Maple Leaf QA tool">QA</button>'
+    menu += '<button class="btn btn-info brdr-rds-0" data-action="folder" title="Open AEM folder view">Folder</button>'
+    menu += '<button class="btn btn-info brdr-rds-0" data-action="editor" title="Open AEM page editor">Editor</button>'
+    menu += '<button class="btn btn-info brdr-rds-0" data-action="properties" title="Open AEM properties editor">Properties</button>'
+    menu += '<button class="btn btn-danger brdr-rds-0" data-action="publish" title="Republish to production">Republish</button>'
     menu += '</li>'
     menu += '</ul>'
 
     $(menu).appendTo('.gcweb-menu .container:first')
-
   }
 
-  function link(type, node) {
-
-    switch (type) {
-      case 'edit': return author + '/editor.html' + node + '.html'
-      case 'prop': return author + '/mnt/overlay/wcm/core/content/sites/properties.html?item=' + node
-      case 'meta': return node + '/_jcr_content.json?_=' + new Date().getTime()
-      case 'publish': return author + '/libs/wcm/core/content/sites/publishpagewizard.html?item=' + node
-      case 'qa': return 'https://caf-fac.ca/tools/ml/qa-single.html?=' + window.location.href
-      case 'folder': return author + '/sites.html' + node.replace(/[^/]+$/i, '')
+  // Available actions
+  var actions = {
+    both: function () {
+      alt = !alt
+      $('#both').text(alt ? 'Both' : 'Single').removeClass(alt ? 'btn-default' : 'btn-danger').addClass(alt ? 'btn-danger' : 'btn-default')
+      GM_setValue('alt', alt)
+    },
+    editor: function () {
+      if (alt) { window.open(server + '/editor.html' + node_alt + '.html') }
+      window.open(server + '/editor.html' + node + '.html')
+    },
+    properties: function () {
+      if (alt) { window.open(server + '/mnt/overlay/wcm/core/content/sites/properties.html?item=' + node_alt + '&autoclose') }
+      window.open(server + '/mnt/overlay/wcm/core/content/sites/properties.html?item=' + node + '&autoclose')
+    },
+    metadata: function () {
+      var t = new Date().getTime()
+      if (alt) { window.open(node_alt + '/_jcr_content.json?_=' + t) }
+      window.open(node + '/_jcr_content.json?_=' + t)
+    },
+    publish: function () {
+      if (alt) { window.open(server + '/libs/wcm/core/content/sites/publishpagewizard.html?item=' + node_alt + '&autoclose' + autoPublish) }
+      window.open(server + '/libs/wcm/core/content/sites/publishpagewizard.html?item=' + node + '&autoclose' + autoPublish)
+    },
+    folder: function () {
+      if (alt) { window.open(server + '/sites.html' + node_alt.replace(/[^/]+$/i, '')) }
+      window.open(server + '/sites.html' + node.replace(/[^/]+$/i, ''))
+    },
+    qa: function () {
+      if (alt) { window.open('https://caf-fac.ca/tools/ml/qa-single.html?=' + node_alt.replace('/content/canadasite/', 'https://www.canada.ca/') + '.html') }
+      window.open('https://caf-fac.ca/tools/ml/qa-single.html?=' + node.replace('/content/canadasite/', 'https://www.canada.ca/') + '.html')
     }
   }
 
-
-  $(document).on('click', '#aem_tools a', function (e) {
-    var i = $(this)
-    var type = i.data('type') || ''
-    var target = i.attr('target') || '_self'
-
-    window.open(link(type, node), target)
-
-    /*if(type !== 'qa' || type!=='edit'){
-        window.open(link(type, node_alt), target)
-        window.open(link(type, node), target)
-    } else {
-        window.open(link(type, node), target)
-    }*/
-
-  })
-
-  function status(cb) {
-    var d = $.Deferred()
-    $.ajax(author, { complete: function (xhr) { d.resolve(xhr.status) } })
-    if (cb) { d.then(cb) }
-    return d
-  }
-
-  status(function (code) {
-    console.log(code)
+  // Handle button events
+  $(document).on('click', '#aem_tools button', function (e) {
+    var name = $(this).data('action')
+    if (name) { actions[name]() }
   })
 
 })(jQuery)
