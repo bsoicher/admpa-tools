b7b787d3da7029987f740fb15e51a080b4af684e
Ben Soicher <ben.soicher@gmail.com>
2020-03-16 23:48:33 -0400

    huge progress on mocha

---
+++ qa/test.js
@@ -0,0 +1,27 @@
+/* global expect, describe, it  */
+
+describe('Hello world', function () {
+  it('should be 11 characters long', function () {
+    var e = 'Hello world'
+    expect(e).to.have.length(11);
+  });
+  it('should be 11 characters long', function () {
+    var e = 'Hello world'
+    expect(e).to.have.length(11);
+  });
+  it('should be 11 characters long', function () {
+    var e = 'Hello world'
+    expect(e).to.have.length(11);
+  });
+  it('should be 11 characters long', function () {
+    var e = 'Hello world'
+    expect(e).to.have.length(11);
+  });
+})
+
+describe('Hello world 2', function () {
+  it('should be 11 characters long', function () {
+    expect('Hello world').to.have.length(11)
+  })
+  it('sgdsergsedrger')
+})
